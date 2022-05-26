00d44d5768be21c3b01efcede7f913a610bd91e2
LeVlad <leca.vlad@gmail.com>
2022-05-26 18:29:56 +0000

    Deploy test 1

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,9 @@ from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
+
+
 class PostAdmin(SummernoteModelAdmin):
+
     prepopulated_fields = {'slug': ('title', )}
     summernote_fields = 'content'
